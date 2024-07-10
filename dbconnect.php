8f679f613cbd262432eed2092db5d97515603f3f
36115 <138181602+36115@users.noreply.github.com>
2024-07-10 14:42:50 +0700

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $database = "bpk_610";
+    $username = "root";
+    $password = "";
+
+    $conn = mysqli_connect($servername, $username, $password, $database) or die("Connection Failed.");
+?>
